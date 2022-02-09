3f18603340d6cf46930cdbce6e3c236a69509aae
Bhavesh Laddagiri <bhavesh.laddagiri1@gmail.com>
2022-02-09 23:34:43 +0530

    update terminal utils for nqueens

--- nqueens/terminal_utils.py
+++ nqueens/terminal_utils.py
@@ -3,32 +3,11 @@ ANSI Codes Reference https://gist.github.com/fnky/458719343aabd01cfb17a3a4f72967
 '''
 
 import sys
-import time
 
 
 def write(text):
     sys.stdout.write(text)
     sys.stdout.flush()
-
-
-def print_error(msg):
-    write(f"\033[31m{msg}\033[0m")
-    time.sleep(2)
-    write("\033[2K\r")
-
-
-def print_info(msg):
-    write(msg)
-    time.sleep(3)
-    write("\033[2K\r")
-
-
-def print_win(player):
-    write(f"🥇Player \033[32m{player}\033[0m Wins!🎉\n\n")
-
-
-def print_tie():
-    write(f"🔀\033[33mGame Tied\033[0m🔀\n\n")
 
 
 def move(n, direction):
