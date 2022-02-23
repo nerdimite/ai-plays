513add29fcf75ffc5077593abb9dd300a084208b
Bhavesh Laddagiri <bhavesh.laddagiri1@gmail.com>
2022-02-23 18:36:05 +0530

    update desc

--- maze_solving/maze_solver.py
+++ maze_solving/maze_solver.py
@@ -3,7 +3,7 @@ from dfs import run as dfs
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Solves the NQueens Problem using Backtracking')
+        description='Solves a Maze using Various Search Algorithms')
     parser.add_argument('-a', '--algo', type=str, default='dfs',
                         choices=['dfs', 'bfs'], help="Search Algorithm to use to solve the Maze. Default is bfs.")
     args = parser.parse_args()
